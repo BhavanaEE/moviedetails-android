e1201ac65f7d6da18b2d78c934932217904e2b44
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-03-19 23:03:33 +0530

    Added data class Movie

---
+++ app/src/main/java/com/example/moviedetailsapplication/ui/Movie.kt
@@ -0,0 +1,15 @@
+package com.example.moviedetailsapplication.ui
+
+import java.util.*
+
+data class Movie (
+    val id: Int,
+    val title: String,
+    val originalLanguage: String,
+    val releaseDate: Date,
+    val overView: String,
+    val imageUrl:String,
+    val voteAverage:Int,
+    val voteCount:Int
+)
+
---
+++ app/src/main/java/com/example/moviedetailsapplication/ui/Movies.kt
@@ -0,0 +1,5 @@
+package com.example.moviedetailsapplication.ui
+
+data class Movies (
+    val listOfMovies:ArrayList<Movie>
+)
