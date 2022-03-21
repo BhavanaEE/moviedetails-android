4e9d28c8b1fcad2cf3c11330a3ba5dc9386221b6
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-03-22 00:44:39 +0530

    Added search function in Movies Interface

--- app/src/main/java/com/example/moviedetailsapplication/network/MoviesInterface.kt
+++ app/src/main/java/com/example/moviedetailsapplication/network/MoviesInterface.kt
@@ -2,12 +2,16 @@ package com.example.moviedetailsapplication.network
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MoviesInterface {
 
-    @GET("movie?sort_by=popularity.desc&api_key=e7d826d84097c8429e815cd8ab31c7a0")
+    @GET("discover/movie?sort_by=popularity.desc&api_key=e7d826d84097c8429e815cd8ab31c7a0")
     fun getMovies(): Call<APIResponse>
 
-    @GET("movie?primary_release_year=2022&api_key=e7d826d84097c8429e815cd8ab31c7a0")
-    fun getCurrentYearMovies(): Call<APIResponse>
+    @GET("discover/movie?primary_release_year=2022&api_key=e7d826d84097c8429e815cd8ab31c7a0")
+    fun getMovieByYear(@Query("query") query:Int): Call<APIResponse>
+
+    @GET("search/movie?api_key=e7d826d84097c8429e815cd8ab31c7a0")
+    fun searchMovie(@Query("query") query: String): Call<APIResponse>
 }
