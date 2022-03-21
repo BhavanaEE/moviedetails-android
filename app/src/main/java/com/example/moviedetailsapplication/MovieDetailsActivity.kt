dc6ba8a44dac1782a8cfcc4121a77de08dbd8c0f
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-03-22 01:13:58 +0530

    Refactored by writing dimens for dimensions and string in resource file

--- app/src/main/java/com/example/moviedetailsapplication/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/moviedetailsapplication/MovieDetailsActivity.kt
@@ -1,15 +1,22 @@
 package com.example.moviedetailsapplication
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
 class MovieDetailsActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_description)
-        val movieNameTv = intent.extras?.getString("movieName")
-        val overViewTv = intent.extras?.getString("overview")
-        var outputString = getString(R.string.displayMessage,movieNameTv, overViewTv)
+        val movieName = intent.extras?.getString(MOVIENAME)
+        val overView = intent.extras?.getString(OVERVIEW)
+        val image = intent.extras?.getString(IMAGEURL)
+        val imageViewTv = findViewById<ImageView>(R.id.imageTv)
+        val imageURL="https://image.tmdb.org/t/p/w200"+image
+        var outputString = getString(R.string.displayMessage,movieName, overView)
+        Glide.with(this).load(imageURL).into(imageViewTv)
         findViewById<TextView>(R.id.movieOverview).setText(outputString)
 
     }
