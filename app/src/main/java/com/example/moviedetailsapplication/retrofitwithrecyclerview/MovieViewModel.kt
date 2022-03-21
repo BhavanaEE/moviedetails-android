c9bdaec3a1e24d4c9de831085e36db249bae635a
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-03-22 00:48:17 +0530

    Added list of searched movies by query functionality

--- app/src/main/java/com/example/moviedetailsapplication/retrofitwithrecyclerview/MovieViewModel.kt
+++ app/src/main/java/com/example/moviedetailsapplication/retrofitwithrecyclerview/MovieViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.moviedetailsapplication.retrofitwithrecyclerview
 
-import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.moviedetailsapplication.network.APIResponse
@@ -13,36 +13,45 @@ import retrofit2.Response
 import java.util.*
 
 class MovieViewModel: ViewModel() {
-    private val _listOfMovies = MutableLiveData(Movies(listOf(Movie(0, "", "", Date(), "", "", 0.0, 0))))
-    private val _listOfCurrentYearMovies = MutableLiveData(Movies(listOf(Movie(0, "", "", Date(), "", "", 0.0, 0))))
+    private val _listOfMovies = MutableLiveData(Movies(listOf(Movie(0, "", "", "", "", "", 0.0, 0))))
+    private val _listOfCurrentYearMovies = MutableLiveData(Movies(listOf(Movie(0, "", "", "", "", "", 0.0, 0))))
+    private val _listOfSearchedMovies = MutableLiveData(Movies(listOf(Movie(0, "", "", "", "", "", 0.0, 0))))
 
-    val listOfMovies: MutableLiveData<Movies> = _listOfMovies
-    val listOfCurrentYearMovies:MutableLiveData<Movies> = _listOfCurrentYearMovies
+    val listOfMovies: LiveData<Movies> = _listOfMovies
+    val listOfCurrentYearMovies:LiveData<Movies> = _listOfCurrentYearMovies
+    val listOfSearchedMovies:LiveData<Movies> = _listOfSearchedMovies
 
     fun getMovies(movieRepository: MovieRepository) {
         val movieResponse=movieRepository.getMovies()
         movieResponse.enqueue(object : Callback<APIResponse> {
             override fun onFailure(call: Call<APIResponse>?, t: Throwable?) {
-                Log.v("retrofit", t?.message.toString())
             }
-
             override fun onResponse(call: Call<APIResponse>?, response: Response<APIResponse>?) {
-                _listOfMovies.postValue(Movies(movieRepository.convertDTOIntoUIModel(response!!.body()!!.movies)))
-                Log.i("data= ", response.body().toString())
+                _listOfMovies.postValue(Movies(movieRepository.convertIntoUIModel(response!!.body()!!.movies)))
             }
         })
     }
 
     fun getCurrentYearMovies(movieRepository: MovieRepository) {
-        val currentYearMovieResponse=movieRepository.getCurrentYearMovies()
+        val calendar = Calendar.getInstance()
+        val year = calendar[Calendar.YEAR]
+        val currentYearMovieResponse=movieRepository.getCurrentYearMovies(year)
         currentYearMovieResponse.enqueue(object : Callback<APIResponse> {
             override fun onFailure(call: Call<APIResponse>?, t: Throwable?) {
-                Log.v("retrofit", t?.message.toString())
             }
-
             override fun onResponse(call: Call<APIResponse>?, response: Response<APIResponse>?) {
-                _listOfCurrentYearMovies.postValue(Movies(movieRepository.convertDTOIntoUIModel(response!!.body()!!.movies)))
-                Log.i("data BHavana= ", response.body().toString())
+                _listOfCurrentYearMovies.postValue(Movies(movieRepository.convertIntoUIModel(response!!.body()!!.movies)))
+            }
+        })
+    }
+
+    fun getListOfSearchedMovies(movieRepository: MovieRepository,query: String){
+        val currentYearMovieResponse=movieRepository.getSearchedMovies(query)
+        currentYearMovieResponse.enqueue(object : Callback<APIResponse> {
+            override fun onFailure(call: Call<APIResponse>?, t: Throwable?) {
+            }
+            override fun onResponse(call: Call<APIResponse>?, response: Response<APIResponse>?) {
+                _listOfSearchedMovies.postValue(Movies(movieRepository.convertIntoUIModel(response!!.body()!!.movies)))
             }
         })
     }
